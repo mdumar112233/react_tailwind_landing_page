f91aa40ad36124af89ac875808fa3e359fe429c4
mdumar112233 <mdumarfaruk187326@gmail.com>
2022-01-30 15:34:07 +0600

    update service

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -7,12 +7,11 @@ import { CgFileDocument } from "react-icons/cg";
 import { FaSolarPanel } from "react-icons/fa";
 import { BsFillGiftFill } from "react-icons/bs";
 
-
 const Services = () => {
   return (
-    <div className="h-[40rem] mt-60 grid lg:grid-cols-3 mx-auto w-11/12">
-
-      <div className="lg:w-80 lg:h-[70%] border border-[#5e6576] pt-12 pl-7 rounded-3xl">
+    <div className="h-[40rem] mt-60 grid col-auto lg:grid-cols-3 w-[80%] mx-auto">
+      
+      <div className="lg:w-80 lg:h-[70%] border border-[#5e6576] pt-12 pl-7 pr-2 rounded-3xl">
         <div className="bg-services-icon-bg-blue bg-no-repeat w-full h-24 text-center">
           <div className="h-24 flex items-center w-20 justify-center">
             <CgFileDocument className="text-white text-[2.2rem] text-center block" />
@@ -44,68 +43,65 @@ const Services = () => {
         </div>
       </div>
 
-      <div className="lg:w-80 lg:h-[70%] border border-[#5e6576] pt-12 pl-7 rounded-3xl">
+      <div className="lg:w-80 lg:h-[70%] border border-[#5e6576] pt-12 pl-7 pr-2 rounded-3xl">
         <div className="bg-services-icon-bg-yellow bg-no-repeat w-full h-24 text-center">
           <div className="h-24 flex items-center w-20 justify-center">
             <FaSolarPanel className="text-white text-[2.2rem] text-center block" />
           </div>
         </div>
-        <h4 className="mt-4 font-medium text-xl">Landing page</h4>
+        <h4 className="mt-4 font-medium text-xl">Corporate site</h4>
         <p className="text-[#5e6576] mt-5">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat.
+          Nam eu erat tellused vivamus vitae sem in tortor pharetra vehicula orn
         </p>
         <div className="flex items-center mt-3">
           <BiCheck className="text-2xl font-extrabold" />
-          <span className="text-[#5e6576]">Phasellus vitae metus in felis</span>
+          <span className="text-[#5e6576]">Vivaus dignissim sit amet nisi</span>
         </div>
         <div className="flex items-center mt-3">
           <BiCheck className="text-2xl font-extrabold" />
           <span className="text-[#5e6576]">
-            Fusce pulvinar eu mi ac molestie
+            Aliqam a tristique nibh in pretium
           </span>
         </div>
         <div className="flex items-center mt-3">
           <BiCheck className="text-2xl font-extrabold" />
-          <span className="text-[#5e6576]">
-            Ut tincidunt vitae enim non vehi
-          </span>
+          <span className="text-[#5e6576]">Nunc commodo magna quis blah</span>
         </div>
         <div className="flex items-center mt-3">
           <BiCheck className="text-2xl font-extrabold" />
-          <span className="text-[#5e6576]">Curabitur consequat nisl eget</span>
+          <span className="text-[#5e6576]">Lacus fermentum tincidunt</span>
         </div>
       </div>
 
-
-      <div className="lg:w-80 lg:h-[70%] border border-[#5e6576] pt-12 pl-7 rounded-3xl">
+      <div className="lg:w-80 lg:h-[70%] border border-[#5e6576] pt-12 pl-7 pr-2  rounded-3xl">
         <div className="bg-services-icon-bg-red bg-no-repeat w-full h-24 text-center">
           <div className="h-24 flex items-center w-20 justify-center">
             <BsFillGiftFill className="text-white text-[2.2rem] text-center block" />
           </div>
         </div>
-        <h4 className="mt-4 font-medium text-xl">Landing page</h4>
+        <h4 className="mt-4 font-medium text-xl">Online shop</h4>
         <p className="text-[#5e6576] mt-5">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat.
         </p>
         <div className="flex items-center mt-3">
           <BiCheck className="text-2xl font-extrabold" />
-          <span className="text-[#5e6576]">Phasellus vitae metus in felis</span>
+          <span className="text-[#5e6576]">Sed laoreet blandit mollis ne</span>
         </div>
         <div className="flex items-center mt-3">
           <BiCheck className="text-2xl font-extrabold" />
           <span className="text-[#5e6576]">
-            Fusce pulvinar eu mi ac molestie
+            Mauris non luctus est quisquerm
           </span>
         </div>
         <div className="flex items-center mt-3">
           <BiCheck className="text-2xl font-extrabold" />
           <span className="text-[#5e6576]">
-            Ut tincidunt vitae enim non vehi
+            Mattis dapibus quisque tristique
           </span>
         </div>
         <div className="flex items-center mt-3">
           <BiCheck className="text-2xl font-extrabold" />
-          <span className="text-[#5e6576]">Curabitur consequat nisl eget</span>
+          <span className="text-[#5e6576]">Cursus lacus interdum sollicdn</span>
         </div>
       </div>
     </div>
