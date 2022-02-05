e62837cd5b80fbc3c1f56269a39e7c9baffec5c0
md umar faruk <mdumarfaruk187326@gmail.com>
2022-02-05 11:40:39 +0000

    new code

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,11 +3,12 @@ import aboutImage from '../../images/details-1.svg';
 
 const About = () => {
     return (
-            <div className='grid  grid-cols-1 lg:grid-cols-2 w-[85%] mx-auto gap-10 lg:mt-28 e_sm:mt-[65rem] mt-[77rem]'>
+            <div className='grid  grid-cols-1 lg:grid-cols-2 w-[85%] mx-auto gap-10 lg:mt-28 e_sm:mt-[65rem] mt-[77rem] border-2 border-red-700'>
             <div className="">
                 <img src={aboutImage} alt="aboutImage" className='lg:w-auto w-4/5 mx-auto' />
             </div>
-            <div className="flex flex-col justify-center w-full lg:w-2/3 mx-auto col-end-auto">
+            {/* <div className='flex justify-center border-2 border-purple-700 '> */}
+            <div className="flex flex-col justify-center border-2 border-red-700 pl-24">
                 <h1 className='text-3xl'>Perfect solution</h1>
                 <h3 className='text-3xl font-bold text-[#222222]'>for your small business</h3>
 
@@ -17,6 +18,7 @@ const About = () => {
 
                 <button className='self-start bg-[#ff5574] px-8 py-2 rounded-full border border-[#ff5574] hover:bg-transparent hover:text-[#ff5574] text-white transition-all mt-5'>Modal</button>
             </div>
+            {/* </div> */}
         </div>
     );
 };
